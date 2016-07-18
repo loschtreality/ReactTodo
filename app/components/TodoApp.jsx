63bc97e2d0c8e1d22620402b85c08c7b6046419a
Michael Loschiavo <losch103@gmail.com>
2016-07-18 11:18:44 -0700

    Created state components and modified webpack

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var TodoApp = React.createClass({
+
+  render: function() {
+    return (
+     <div>
+       <span>Todo app.jsx</span>
+     </div>
+    );
+  }
+
+});
+
+module.exports = TodoApp;
