aa9f0c06d0ea605fed5788fc7646fe27123378fd
Michael Loschiavo <losch103@gmail.com>
2016-07-19 14:36:47 -0700

    Complete handleAddTodo

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -11,4 +11,16 @@ describe("TodoApp", () => {
   it("should exist", () => {
     expect(TodoApp).toExist()
   })
+
+
+  it("should add todo to the todos state on handleAddTodo", () => {
+    var todoText = "Take a shower"
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp/>)
+
+    todoApp.setState({todos: []})
+    todoApp.handleAddTodo(todoText)
+
+    expect(todoApp.state.todos[0].text).toBe(todoText)
+
+  })
 })
