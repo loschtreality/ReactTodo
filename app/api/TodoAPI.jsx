051ddc3d5b318d7398937c7030636c2706c67420
Michael Loschiavo <losch103@gmail.com>
2016-07-20 11:33:35 -0700

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -17,5 +17,33 @@ module.exports = {
 
     }
     return $.isArray(todos) ? todos : []
+  },
+  filterTodos: function (todos, showCompleted, searchText) {
+
+    // Filter by showCompleted
+    // Filter by searchText
+    // Sort todos with non-completed first
+
+    return todos.filter((el) => {
+      return !el.completed || showCompleted
+    })
+    .filter((el) => {
+      if (el.text.toLowerCase().indexOf(searchText) > -1 || searchText.length === 0) {
+        return el
+      }
+    })
+    .sort((a,b) => {
+      if (!a.completed && b.completed) {
+        return -1
+      }
+      else if (a.completed && !b.completed) {
+        return 1
+      }
+      else {
+        return 0
+      }
+    })
+
+
   }
 };
--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -30,4 +30,5 @@ describe("TodoList", () => {
   expect(todosComponents.length).toBe(todos.length)
   })
 
+
 })
